eff8486955e0bcaa18d769824b8cfd549c9e2a2b
Alice <coolalisa@yandex.ru>
2019-04-12 19:24:50 +0300

    Move spinner to ActionBar
    Add spinner item to menu, add spinner_item and spinner_dropdown_item for displaying spaces between cities.

--- app/src/main/java/com/alicedmitrieva/weatherapp/MainActivity.java
+++ app/src/main/java/com/alicedmitrieva/weatherapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.alicedmitrieva.weatherapp;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,11 +23,18 @@ public class MainActivity extends AppCompatActivity implements RespondWeatherDat
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewPager);
-        spinner = findViewById(R.id.spinner);
+        startSendingRequest();
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.android_action_bar_spinner_menu, menu);
+
+        spinner = (Spinner) menu.findItem(R.id.spinner).getActionView();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.spinner_item,
                 getResources().getStringArray(R.array.cities));
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -40,7 +48,9 @@ public class MainActivity extends AppCompatActivity implements RespondWeatherDat
 
             }
         });
-        startSendingRequest();
+
+        spinner.setAdapter(adapter);
+        return true;
     }
 
     @Override
@@ -54,6 +64,8 @@ public class MainActivity extends AppCompatActivity implements RespondWeatherDat
     }
 
     void startSendingRequest() {
-        new RespondWeatherDataTask(MainActivity.this, spinner.getSelectedItem().toString()).execute();
+        if (spinner != null) {
+            new RespondWeatherDataTask(MainActivity.this, spinner.getSelectedItem().toString()).execute();
+        }
     }
 }

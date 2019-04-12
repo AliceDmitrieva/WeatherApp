8fd9cfe08b10e85dce566f8de4c22572af76a34c
Alice <coolalisa@yandex.ru>
2019-04-12 16:36:05 +0300

    Add spinner with city names

--- app/src/main/java/com/alicedmitrieva/weatherapp/MainActivity.java
+++ app/src/main/java/com/alicedmitrieva/weatherapp/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RespondWeatherDataTask.WeatherDataListener {
 
     ViewPager viewPager;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity implements RespondWeatherDat
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewPager);
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.cities));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         new RespondWeatherDataTask(MainActivity.this).execute();
     }
 
